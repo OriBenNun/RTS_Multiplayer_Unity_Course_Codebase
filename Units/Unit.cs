5a2e270b6ea4b9053e463ab5aeae0ed8ef7834bd
BmanStudio <57292081+BmanStudio@users.noreply.github.com>
2023-04-06 21:56:41 +0300

    Created basic unit select and deselect

---
+++ Units/Unit.cs
@@ -0,0 +1,32 @@
+﻿using Mirror;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace Units
+{
+    public class Unit : NetworkBehaviour
+    {
+        [SerializeField] private UnityEvent onSelected = null;
+        [SerializeField] private UnityEvent onDeselected = null;
+
+        #region Client
+        
+        [Client]
+        public void Select()
+        {
+            if (!hasAuthority) { return; }
+            
+            onSelected?.Invoke();
+        }
+        
+        [Client]
+        public void Deselect()
+        {
+            if (!hasAuthority) { return; }
+            
+            onDeselected?.Invoke();
+        }
+        #endregion
+
+    }
+}
---
+++ Units/UnitSelectionHandler.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using Mirror;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace Units
+{
+    public class UnitSelectionHandler : MonoBehaviour
+    {
+        [SerializeField] private LayerMask layerMask = new LayerMask();
+        
+        private Camera _mainCamera;
+
+        private List<Unit> _selectedUnits = new List<Unit>();
+        
+        private void Start()
+        {
+            _mainCamera = Camera.main;
+        }
+
+        private void Update()
+        {
+            if (Mouse.current.leftButton.wasPressedThisFrame)
+            {
+                // DeselectAllUnits();
+            }
+            else if (Mouse.current.leftButton.wasReleasedThisFrame)
+            {
+                DeselectAllUnits();
+                ClearSelectionArea();
+            }
+        }
+
+        private void ClearSelectionArea()
+        {
+            var ray = _mainCamera.ScreenPointToRay(Mouse.current.position.ReadValue());
+
+            if (!Physics.Raycast(ray, out var hit, Mathf.Infinity, layerMask)) { return; }
+
+            if (!hit.collider.TryGetComponent<Unit>(out var unit)) { return; }
+
+            if (!unit.hasAuthority) { return; }
+            
+            _selectedUnits.Add(unit);
+
+            foreach (var selectedUnit in _selectedUnits)
+            {
+                selectedUnit.Select();
+            }
+        }
+
+        private void DeselectAllUnits()
+        {
+            foreach (var selectedUnit in _selectedUnits)
+            {
+                selectedUnit.Deselect();
+            }
+            
+            _selectedUnits.Clear();
+        }
+    }
+}
