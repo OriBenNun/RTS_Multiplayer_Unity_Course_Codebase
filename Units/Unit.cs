20a5f5af849587ccbb13a53178a9018d93baae7d
BmanStudio <57292081+BmanStudio@users.noreply.github.com>
2023-04-08 16:58:55 +0300

    Added RtsPlayer to handle the storing of which units belong to which player, for future multi-selection

--- Units/Unit.cs
+++ Units/Unit.cs
@@ -1,4 +1,5 @@
-﻿using Mirror;
+﻿using System;
+using Mirror;
 using UnityEngine;
 using UnityEngine.Events;
 
@@ -7,13 +8,45 @@ namespace Units
     public class Unit : NetworkBehaviour
     {
         [SerializeField] private UnitMovement unitMovement;
-        [SerializeField] private UnityEvent onSelected = null;
-        [SerializeField] private UnityEvent onDeselected = null;
+        [SerializeField] private UnityEvent onSelected;
+        [SerializeField] private UnityEvent onDeselected;
+
+        public static event Action<Unit> ServerOnUnitSpawned;
+        public static event Action<Unit> ServerOnUnitDespawned;
+        public static event Action<Unit> AuthorityOnUnitSpawned;
+        public static event Action<Unit> AuthorityOnUnitDespawned;
 
         public UnitMovement GetUnitMovement() { return unitMovement; }
+
+        #region Server
+
+        public override void OnStartServer()
+        {
+            ServerOnUnitSpawned?.Invoke(this);
+        }
+
+        public override void OnStopServer()
+        {
+            ServerOnUnitDespawned?.Invoke(this);
+        }
+
+        #endregion
         
         #region Client
-        
+
+        public override void OnStartClient()
+        {
+            if (!isClientOnly || !hasAuthority) { return; } // `!isClientOnly` Prevents the Host from running to avoid duplicated lists
+            
+            AuthorityOnUnitSpawned?.Invoke(this);
+        }
+        public override void OnStopClient()
+        {
+            if (!isClientOnly || !hasAuthority) { return; } // `!isClientOnly` Prevents the Host from running to avoid duplicated lists
+            
+            AuthorityOnUnitDespawned?.Invoke(this);
+        }
+
         [Client]
         public void Select()
         {
@@ -30,6 +63,6 @@ namespace Units
             onDeselected?.Invoke();
         }
         #endregion
-
+        
     }
 }
