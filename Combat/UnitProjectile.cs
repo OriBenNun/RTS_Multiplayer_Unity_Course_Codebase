b12eb6ab8323132898a2500498ece4eef531888d
BmanStudio <57292081+BmanStudio@users.noreply.github.com>
2023-04-10 02:54:59 +0300

    WIP - started adding health and damage mechanics

--- Combat/UnitProjectile.cs
+++ Combat/UnitProjectile.cs
@@ -9,6 +9,7 @@ namespace Combat
         [SerializeField] private Rigidbody rb;
         [SerializeField] private float destroyAfterSeconds = 5f;
         [SerializeField] private float launchForce = 10f;
+        [SerializeField] private int damageToDeal = 20;
 
         private void Start()
         {
@@ -18,6 +19,21 @@ namespace Combat
         public override void OnStartServer()
         {
             Invoke(nameof(DestroySelf), destroyAfterSeconds);
+        }
+
+        [ServerCallback]
+        private void OnTriggerEnter(Collider other)
+        {
+            if (!other.TryGetComponent<NetworkIdentity>(out var networkIdentity)) return;
+            
+            if (networkIdentity.connectionToClient == connectionToClient) { return; }
+
+            if (other.TryGetComponent<Health>(out var targetHealth))
+            {
+                targetHealth.DealDamage(damageToDeal);
+            }
+            
+            DestroySelf();
         }
 
         [Server]
