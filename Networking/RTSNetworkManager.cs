6a404d17c90c76ed5ed307916291696132059503
BmanStudio <57292081+BmanStudio@users.noreply.github.com>
2023-04-11 00:52:00 +0300

    Instantiating the gameOverHandlerPrefab when it's the game's scene

--- Networking/RTSNetworkManager.cs
+++ Networking/RTSNetworkManager.cs
@@ -1,5 +1,7 @@
 ﻿using Mirror;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Serialization;
 
 namespace Networking
 {
@@ -7,7 +9,9 @@ namespace Networking
     {
 
         [Header("Additional Custom Settings")]
-        [SerializeField] private GameObject unitSpawnerPrefab;
+        [SerializeField] private GameObject unitSpawnerPrefab; 
+        [SerializeField] private GameOverHandler gameOverHandlerPrefab;
+        
         public override void OnServerAddPlayer(NetworkConnection conn)
         {
             base.OnServerAddPlayer(conn);
@@ -16,6 +20,15 @@ namespace Networking
             var unitSpawnerInstance = Instantiate(unitSpawnerPrefab, playerTransform.position, playerTransform.rotation);
             
             NetworkServer.Spawn(unitSpawnerInstance, conn);
+        }
+
+        public override void OnServerSceneChanged(string sceneName)
+        {
+            if (!SceneManager.GetActiveScene().name.StartsWith("Scene_Map")) return;
+            
+            var gameOverHandlerInstance = Instantiate(gameOverHandlerPrefab);
+                
+            NetworkServer.Spawn(gameOverHandlerInstance.gameObject);
         }
     }
 }
