24a25d7ac41b91905e92d6b6a7a2ddf268bd01da
BmanStudio <57292081+BmanStudio@users.noreply.github.com>
2023-04-11 03:13:43 +0300

    Added FaceCamera logic for UI elements

---
+++ Cameras/FaceCamera.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using UnityEngine;
+
+namespace Cameras
+{
+    public class FaceCamera : MonoBehaviour
+    {
+        private Transform _mainCameraTransform;
+
+        private void Start()
+        {
+            if (Camera.main != null) _mainCameraTransform = Camera.main.transform;
+        }
+
+        private void LateUpdate()
+        {
+            var camRotation = _mainCameraTransform.rotation;
+            transform.LookAt(transform.position + camRotation * Vector3.forward, camRotation * Vector3.up);
+        }
+    }
+}
