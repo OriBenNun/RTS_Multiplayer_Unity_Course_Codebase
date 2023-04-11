75505d76e5cef91015064da77c6a91fbb59611f1
BmanStudio <57292081+BmanStudio@users.noreply.github.com>
2023-04-11 20:07:19 +0300

    Added RTS camera controls and cinemachine virtual camera for each player

---
+++ Cameras/CameraController.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using Inputs;
+using Mirror;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace Cameras
+{
+    public class CameraController : NetworkBehaviour
+    {
+        [SerializeField] private Transform playerCameraTransform;
+        [SerializeField] private float speed = 20f;
+        [SerializeField] private float screenBorderThickness = 10f;
+        [SerializeField] private Vector2 screenXLimits = Vector2.zero;
+        [SerializeField] private Vector2 screenZLimits = Vector2.zero;
+        
+        private Controls _controls;
+
+        private Vector2 _prevInputs;
+        public override void OnStartAuthority()
+        {
+            playerCameraTransform.gameObject.SetActive(true);
+
+            _controls = new Controls();
+
+            _controls.Player.MoveCamera.performed += SetPreviousInput;
+            _controls.Player.MoveCamera.canceled += SetPreviousInput;
+            
+            _controls.Enable();
+        }
+
+        [ClientCallback]
+        private void Update()
+        {
+            if (!hasAuthority || !Application.isFocused) { return; }
+
+            UpdateCameraPosition();
+        }
+
+        private void UpdateCameraPosition()
+        {
+            var pos = playerCameraTransform.position;
+
+            if (_prevInputs == Vector2.zero)
+            {
+                var cursorMovement = Vector3.zero;
+                var cursorPos = Mouse.current.position.ReadValue();
+
+                if (cursorPos.y >= Screen.height - screenBorderThickness)
+                {
+                    cursorMovement.z += 1;
+                }
+                else if (cursorPos.y <= screenBorderThickness)
+                {
+                    cursorMovement.z -= 1;
+                }
+                
+                if (cursorPos.x >= Screen.width - screenBorderThickness)
+                {
+                    cursorMovement.x += 1;
+                }
+                else if (cursorPos.x <= screenBorderThickness)
+                {
+                    cursorMovement.x -= 1;
+                }
+
+                pos += cursorMovement.normalized * (speed * Time.deltaTime);
+            }
+            else
+            {
+                pos += new Vector3(_prevInputs.x, 0f, _prevInputs.y) * (speed * Time.deltaTime);
+            }
+
+            pos.x = Mathf.Clamp(pos.x, screenXLimits.x, screenXLimits.y);
+            pos.z = Mathf.Clamp(pos.z, screenZLimits.x, screenZLimits.y);
+
+            playerCameraTransform.position = pos;
+        }
+
+        private void SetPreviousInput(InputAction.CallbackContext ctx) => _prevInputs = ctx.ReadValue<Vector2>();
+    }
+}
---
+++ Inputs/Controls.cs
@@ -0,0 +1,260 @@
+// GENERATED AUTOMATICALLY FROM 'Assets/Inputs/Controls_InputAction.inputactions'
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Utilities;
+
+namespace Inputs
+{
+    public class @Controls : IInputActionCollection, IDisposable
+    {
+        public InputActionAsset asset { get; }
+        public @Controls()
+        {
+            asset = InputActionAsset.FromJson(@"{
+    ""name"": ""Controls_InputAction"",
+    ""maps"": [
+        {
+            ""name"": ""Player"",
+            ""id"": ""b752fc2e-4f10-44ea-b0e5-eeb9d4fafbba"",
+            ""actions"": [
+                {
+                    ""name"": ""Move Camera"",
+                    ""type"": ""Value"",
+                    ""id"": ""8e8992e2-63e5-4823-b9c4-f9a932995472"",
+                    ""expectedControlType"": ""Vector2"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                }
+            ],
+            ""bindings"": [
+                {
+                    ""name"": ""WASD"",
+                    ""id"": ""c73d62f8-fa22-4c02-99e6-9359a716967b"",
+                    ""path"": ""2DVector"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Move Camera"",
+                    ""isComposite"": true,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": ""up"",
+                    ""id"": ""2f1293cd-95a2-49e5-9358-7ed097e5707b"",
+                    ""path"": ""<Keyboard>/w"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": ""Keyboard & Mouse"",
+                    ""action"": ""Move Camera"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""down"",
+                    ""id"": ""38b3e7d6-5c20-4dbc-8282-d0800ee89aab"",
+                    ""path"": ""<Keyboard>/s"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": ""Keyboard & Mouse"",
+                    ""action"": ""Move Camera"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""left"",
+                    ""id"": ""f780a806-e49f-487d-a474-2de32d4524f3"",
+                    ""path"": ""<Keyboard>/a"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": ""Keyboard & Mouse"",
+                    ""action"": ""Move Camera"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""right"",
+                    ""id"": ""0d106208-a511-4cd3-a247-e5ef15b25ebd"",
+                    ""path"": ""<Keyboard>/d"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": ""Keyboard & Mouse"",
+                    ""action"": ""Move Camera"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""Arrow Keys"",
+                    ""id"": ""b79d8479-619d-4333-b0bc-559c7931e835"",
+                    ""path"": ""2DVector"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Move Camera"",
+                    ""isComposite"": true,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": ""up"",
+                    ""id"": ""4910a475-764e-4608-a057-c7cfcb38b9b3"",
+                    ""path"": ""<Keyboard>/upArrow"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": ""Keyboard & Mouse"",
+                    ""action"": ""Move Camera"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""down"",
+                    ""id"": ""e9609c85-23ab-432a-ab71-8bdf5a653256"",
+                    ""path"": ""<Keyboard>/downArrow"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": ""Keyboard & Mouse"",
+                    ""action"": ""Move Camera"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""left"",
+                    ""id"": ""cff7f818-c12e-41fd-88f0-cbb70a8a589a"",
+                    ""path"": ""<Keyboard>/leftArrow"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": ""Keyboard & Mouse"",
+                    ""action"": ""Move Camera"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""right"",
+                    ""id"": ""718a6c69-fef0-4e7a-b1b0-b0dbb44f9781"",
+                    ""path"": ""<Keyboard>/rightArrow"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": ""Keyboard & Mouse"",
+                    ""action"": ""Move Camera"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                }
+            ]
+        }
+    ],
+    ""controlSchemes"": [
+        {
+            ""name"": ""Keyboard & Mouse"",
+            ""bindingGroup"": ""Keyboard & Mouse"",
+            ""devices"": [
+                {
+                    ""devicePath"": ""<Keyboard>"",
+                    ""isOptional"": false,
+                    ""isOR"": false
+                },
+                {
+                    ""devicePath"": ""<Mouse>"",
+                    ""isOptional"": false,
+                    ""isOR"": false
+                }
+            ]
+        }
+    ]
+}");
+            // Player
+            m_Player = asset.FindActionMap("Player", throwIfNotFound: true);
+            m_Player_MoveCamera = m_Player.FindAction("Move Camera", throwIfNotFound: true);
+        }
+
+        public void Dispose()
+        {
+            UnityEngine.Object.Destroy(asset);
+        }
+
+        public InputBinding? bindingMask
+        {
+            get => asset.bindingMask;
+            set => asset.bindingMask = value;
+        }
+
+        public ReadOnlyArray<InputDevice>? devices
+        {
+            get => asset.devices;
+            set => asset.devices = value;
+        }
+
+        public ReadOnlyArray<InputControlScheme> controlSchemes => asset.controlSchemes;
+
+        public bool Contains(InputAction action)
+        {
+            return asset.Contains(action);
+        }
+
+        public IEnumerator<InputAction> GetEnumerator()
+        {
+            return asset.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        public void Enable()
+        {
+            asset.Enable();
+        }
+
+        public void Disable()
+        {
+            asset.Disable();
+        }
+
+        // Player
+        private readonly InputActionMap m_Player;
+        private IPlayerActions m_PlayerActionsCallbackInterface;
+        private readonly InputAction m_Player_MoveCamera;
+        public struct PlayerActions
+        {
+            private @Controls m_Wrapper;
+            public PlayerActions(@Controls wrapper) { m_Wrapper = wrapper; }
+            public InputAction @MoveCamera => m_Wrapper.m_Player_MoveCamera;
+            public InputActionMap Get() { return m_Wrapper.m_Player; }
+            public void Enable() { Get().Enable(); }
+            public void Disable() { Get().Disable(); }
+            public bool enabled => Get().enabled;
+            public static implicit operator InputActionMap(PlayerActions set) { return set.Get(); }
+            public void SetCallbacks(IPlayerActions instance)
+            {
+                if (m_Wrapper.m_PlayerActionsCallbackInterface != null)
+                {
+                    @MoveCamera.started -= m_Wrapper.m_PlayerActionsCallbackInterface.OnMoveCamera;
+                    @MoveCamera.performed -= m_Wrapper.m_PlayerActionsCallbackInterface.OnMoveCamera;
+                    @MoveCamera.canceled -= m_Wrapper.m_PlayerActionsCallbackInterface.OnMoveCamera;
+                }
+                m_Wrapper.m_PlayerActionsCallbackInterface = instance;
+                if (instance != null)
+                {
+                    @MoveCamera.started += instance.OnMoveCamera;
+                    @MoveCamera.performed += instance.OnMoveCamera;
+                    @MoveCamera.canceled += instance.OnMoveCamera;
+                }
+            }
+        }
+        public PlayerActions @Player => new PlayerActions(this);
+        private int m_KeyboardMouseSchemeIndex = -1;
+        public InputControlScheme KeyboardMouseScheme
+        {
+            get
+            {
+                if (m_KeyboardMouseSchemeIndex == -1) m_KeyboardMouseSchemeIndex = asset.FindControlSchemeIndex("Keyboard & Mouse");
+                return asset.controlSchemes[m_KeyboardMouseSchemeIndex];
+            }
+        }
+        public interface IPlayerActions
+        {
+            void OnMoveCamera(InputAction.CallbackContext context);
+        }
+    }
+}
